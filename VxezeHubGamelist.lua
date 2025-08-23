b255f7d4fed98cf34dce4bb411560e9cf75cb536
Dex Skibidi <anhthinhcz@gmail.com>
2025-08-23 17:27:12 +0700

    Update VxezeHubGamelist.lua

--- VxezeHubGamelist.lua
+++ VxezeHubGamelist.lua
@@ -22,7 +22,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/ref
 elseif placeId == 79546208627805 or placeId == 126509999114328 then
 --99DaysInTheForest
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHub99daysintheforest.lua"))()
-elseif placeId == 17409568426 or placeId == 18118575275 then
+elseif placeId == 96342491571673 then
 --StealaBranrot
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/Vxezehub/refs/heads/main/VxezeHubStealaBranrot.lua"))()
 elseif placeId == 103754275310547 or placeId == 86076978383613 then
