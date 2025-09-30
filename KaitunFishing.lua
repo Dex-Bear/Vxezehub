8f161f32040161740781af319ce59fba5ff14d3c
Dex Skibidi <anhthinhcz@gmail.com>
2025-09-30 08:48:31 +0700

    Update KaitunFishing.lua

--- KaitunFishing.lua
+++ KaitunFishing.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeUNC/refs/heads/main/NotifyBloxFruit.lua"))()
 loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/1ec099ad296f9bce"))()
